01388ff558f66161093976278b653889911be66a
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-05-19 19:23:08 -0300

    drawble layout created

--- app/src/main/java/com/example/washcar/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/washcar/homeFragment/HomeFragment.kt
@@ -1,15 +1,17 @@
 package com.example.washcar.homeFragment
 
+import android.app.FragmentBreadCrumbs
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.washcar.R
+import com.example.washcar.databinding.FragmentBlankBinding
 
 
 class HomeFragment : Fragment() {
 
+    lateinit var binding: FragmentBlankBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +22,20 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_blank,container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+
+        setHasOptionsMenu(true)
+        return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.navigation_drawer, menu)
+    }
+
+
 
 
 }
