de628420321505942179a542099bea6172f20cab
FilipeMaiaBR45 <58606133+FilipeMaiaBR45@users.noreply.github.com>
2022-07-11 20:47:38 -0300

    update fildes into CustomerResponse.kt

--- app/src/main/java/com/example/washcar/api/customer/model/CustomerResponse.kt
+++ app/src/main/java/com/example/washcar/api/customer/model/CustomerResponse.kt
@@ -3,20 +3,17 @@ package com.example.washcar.api.customer.model
 import com.google.gson.annotations.SerializedName
 
 data class CustomerResponse(
-     @SerializedName("completeName")
-     var completeName : String,
-     @SerializedName("")
-     var email : String,
-     @SerializedName("email")
-     var cpf : String,
-     @SerializedName("phoneNumber")
-     var phoneNumber : String,
-     @SerializedName("cep")
-     var cep : String,
      @SerializedName("id")
      var id : String,
-     @SerializedName("createdAt")
-     var createdAt : String,
-     @SerializedName("updateAt")
-     var updateAt : String
+     @SerializedName("nomeCompleto")
+     var completeName : String,
+     @SerializedName("email")
+     var email : String,
+     @SerializedName("cpf")
+     var cpf : String,
+     @SerializedName("cep")
+     var cep : String,
+     @SerializedName("statusCliente")
+     var statusCliente : String,
+
  )
